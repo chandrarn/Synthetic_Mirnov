07e10bde1d3121ba9bacc43cf865cc8e3f21a79e
Rian Chandra <rian@vbox.hsd1.wa.comcast.net>
2025-01-09 16:55:39 -0500

    Synch

--- gen_MAGX_Coords.py
+++ gen_MAGX_Coords.py
@@ -73,7 +73,7 @@ def gen_Node_Dict():
                     for sig in ['GAIN','NA','R','Z','PHI','POLARITY']:
                         tag = 'SIGNALS.%s.%s.%s%d.%s'%(node_name,name_phi,hv,i,sig)
                         try:nodes[node_name][name_phi]['%s%d'%(hv,i)][sig] = \
-                            float(tree.getNode(tag).getFloatArray())
+                            np.array(tree.getNode(tag).getFloatArray(),dtype=float)
                         except:
                             print('Failure at: %s'%tag)
                             raise SyntaxError
