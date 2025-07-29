1ccf1ab83d6cb2ee070b0d6652b60b40769227c5
Rian Chandra <rianc@uw.edu>
2025-07-29 14:53:20 -0400

    Patience, yung padawan

--- signal_generation/FAR3D_to_Bnorm.py
+++ signal_generation/FAR3D_to_Bnorm.py
@@ -2,21 +2,26 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb 19 15:19:50 2025
-    FAR-3D to b-norm
+    FAR-3D to b-norm for effective surface current calculation
+    Note: the original eigenvector is in normalized-Psi, we 
+    need to first resample it on the original R,Z equilibrium grid
+
+    This assumes that B_r, B_th don't meaningfully transform in direction, magnitude
+    The Merezhkin correction is used to "fake" flux expansion on the LFS. 
+    This does not account for poloidally asymmetric changes in magnitude
 @author: rian
 """
 import numpy as np
 from scipy.interpolate import griddata
 import xarray as xr
-from M3DC1_to_Bnorm import calculate_normal_vector, save_Bnorm
-import matplotlib.pyplot as plt
+try:from M3DC1_to_Bnorm import calculate_normal_vector, save_Bnorm
+except:pass
 from freeqdsk import geqdsk
-from sys import path; path.append('/home/rianc/Documents/eqtools-1.0/')
-import eqtools as eq
 import cv2
+import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
-
+#####################################################################################
 def convert_FAR3D_to_Bnorm(br_file='br_0000',bth_file='bth_0000',\
     m=[9,10,11,12],eqdsk_file='g1051202011.1000',debug=True,lambda_merezhkin=0.4):
     # Pull in FAR3D output eigenvectors, (sin/cos), convery to equilibrium geometry, convery to B-Norm
@@ -25,18 +30,48 @@ def convert_FAR3D_to_Bnorm(br_file='br_0000',bth_file='bth_0000',\
 
     # Restructure r,theta for the cos/sin basis functions into a single B field vector
 
-def psi_norm_conversion(psi_norm,psiC1_file,psi_LCFS):
-    # generate radial vector to map Psi-norm to R
-    psi = xr.load_dataarray(psiC1_file)
-    R = psi.coords['R'].data
-    Z = psi.coords['Z'].data
-    psi = psi.data
+###################################################################################
+def plot_B(br_file='br_0000',bth_file='bth_0000',m=[9,10,11,12],\
+           eqdsk_file='g1051202011.1000',data_directory='../M3D-C1_Data/',plot_directory='../output_plots/'):
+    # Plot FAR3D Eigenfunction output _without_ accounting for equilibrium shaping
+
+    # Load in Data
+    dat_r = np.loadtxt('../M3D-C1_Data/'+br_file,skiprows=1)
+    dat_th = np.loadtxt('../M3D-C1_Data/'+bth_file,skiprows=1)
     
-    psi_max = np.max(psi)
+    psi_norm = dat_r[:,0]
+    dat_r = dat_r[:,1:]
+    dat_th = dat_th[:,1:]
     
-    psi_mid = psi[:,100]
-    psi_n = np.sqrt( (psi_max-psi_mid)/(psi_max-psi_LCFS))
+    # Evolve eigenvector around in a circle, with the appropriate m#
+    theta = np.linspace(0,2*np.pi,100)
+    def eigenfunc(theta,B,m):
+        out = np.zeros((len(B),))
+        for ind,m_ in enumerate(m): out += B[:,2*ind]*np.cos(m_*theta) + B[:,2*ind+1]*np.sin(m_*theta)
+        return out
     
+    r_grid = np.array([eigenfunc(t,dat_r,m) for t in theta]).T
+    th_grid = np.array([eigenfunc(t,dat_th,m) for t in theta]).T
+    
+
+
+    plt.close('FAR3D_Eigenfunction_Original')
+    fig,ax = plt.subplots(1,2,tight_layout=True,num='FAR3D_Eigenfunction_Original',subplot_kw={'projection': 'polar'},figsize=(4,4))
+
+    #ax[0].contourf(theta,psi_normalized,r_grid)
+    #ax[1].contourf(theta,psi_normalized,th_grid)
+    ax[0].contourf(theta,psi_norm,r_grid)
+    ax[1].contourf(theta,psi_norm,th_grid)
+    
+    ax[1].set_xlabel(r'B$_\theta(\psi_N)$')
+    ax[0].set_xlabel(r'B$_r(\psi_N)$')
+    for i in range(2):
+        ax[i].set_xticklabels([])
+        ax[i].set_yticklabels([None,0.25,None,None,1])
+    
+    fig.savefig(plot_directory+'FAR3D_Eigenfunction_Original.pdf',transparent=True)
+    plt.show()
+########################################################################################
 # Get Psi grid, r,z from gEqdsk file
 def get_psi_grid(eqdsk_file):
     with open(eqdsk_file,'r') as f: eqdsk_obj = geqdsk.read(f)
@@ -98,67 +133,25 @@ def interpolate_B(theta_inside,R_inside,Z_inside,dat_B,indices,m,phase=0):
     return R_downsamp, Z_downsamp ,B_RZ_downsamp
 ############
 
-def plot_B(br_file='br_0000',bth_file='bth_0000',m=[9,10,11,12],eqdsk_file='g1051202011.1000'):
-    # Plot FAR3D Eigenfunction output _without_ accounting for equilibrium shaping
-
-    dat_r = np.loadtxt('../M3D-C1_Data/'+br_file,skiprows=1)
-    dat_th = np.loadtxt('../M3D-C1_Data/'+bth_file,skiprows=1)
-    
-    psi_norm = dat_r[:,0]
-    dat_r = dat_r[:,1:]
-    dat_th = dat_th[:,1:]
-    
-    #psi_normalized = get_psi_grid('input_data/'+eqdsk_file)
-
-    #psi_norm = get_psi_grid('input_data/'+eqdsk_file)
-    
-    theta = np.linspace(0,2*np.pi,100)
-    def eigenfunc(theta,B,m):
-        out = np.zeros((len(B),))
-        for ind,m_ in enumerate(m): out += B[:,2*ind]*np.cos(m_*theta) + B[:,2*ind+1]*np.sin(m_*theta)
-        return out
-    
-    r_grid = np.array([eigenfunc(t,dat_r,m) for t in theta]).T
-    th_grid = np.array([eigenfunc(t,dat_th,m) for t in theta]).T
-    
-
-
-    plt.close('FAR3D_Eigenfunction_Original')
-    fig,ax = plt.subplots(1,2,tight_layout=True,num='FAR3D_Eigenfunction_Original',subplot_kw={'projection': 'polar'},figsize=(4,4))
-
-    #ax[0].contourf(theta,psi_normalized,r_grid)
-    #ax[1].contourf(theta,psi_normalized,th_grid)
-    ax[0].contourf(theta,psi_norm,r_grid)
-    ax[1].contourf(theta,psi_norm,th_grid)
-    
-    ax[1].set_xlabel(r'B$_\theta(\psi_N)$')
-    ax[0].set_xlabel(r'B$_r(\psi_N)$')
-    for i in range(2):
-        ax[i].set_xticklabels([])
-        ax[i].set_yticklabels([None,0.25,None,None,1])
-    
-    fig.savefig('../output_plots/FAR3D_Eigenfunction_Original.pdf',transparent=True)
-    plt.show()
-
 ##############################################
 def map_psi_to_equilibrium(br_file='br_0000',bth_file='bth_0000',\
-    m=[9,10,11,12],eqdsk_file='g1051202011.1000',debug=True,lambda_merezhkin=0.4):
+    m=[9,10,11,12],eqdsk_file='g1051202011.1000',debug=True,lambda_merezhkin=0.4,\
+        data_directory='../M3D-C1_Data/',plot_directory='../output_plots/'):
     
     # Load in FAR3D data
-    dat_r = np.loadtxt('../M3D-C1_Data/'+br_file,skiprows=1)
-    dat_th = np.loadtxt('../M3D-C1_Data/'+bth_file,skiprows=1)
+    dat_r = np.loadtxt(data_directory+br_file,skiprows=1)
+    dat_th = np.loadtxt(data_directory+bth_file,skiprows=1)
     
     psi_norm = dat_r[:,0]
     dat_r = dat_r[:,1:]
     dat_th = dat_th[:,1:]
     
     # Load in equilibrium data
-    psi_normalized,R,Z, rmagx,zmagx = get_psi_grid('input_data/'+eqdsk_file)
+    psi_normalized,R,Z, rmagx,zmagx = get_psi_grid(data_directory+eqdsk_file)
 
     # Get plasma boundary contour
     inside_inds,_ = gen_psi_boundary_contour(psi_normalized, R, Z,rmagx,zmagx,psi_LCFS=0.95)
-    if debug: plt.figure();plt.plot(R[inside_inds[:,0]]+rmagx,Z[inside_inds[:,1]]+zmagx,'k*',label='Plasma Domain')
-    
+
     # Regrid R,Z, calculate theta from the magnetic axis
     R_grid, Z_grid = np.meshgrid(R,Z,indexing='ij')
     theta = np.atan2(Z_grid,R_grid) % (2*np.pi) # theta in [0,2pi], atan2 originally in [-pi,pi]
@@ -181,9 +174,9 @@ def map_psi_to_equilibrium(br_file='br_0000',bth_file='bth_0000',\
 
     # Interpolate the FAR3D Mode on the equilibrium Psi grid
     R_downsamp,Z_downsamp, B_r_RZ_downsamp_cos = interpolate_B(theta_inside,R_inside,Z_inside,dat_r[:,0:],indices,m)
-    _,_, B_r_RZ_downsamp_sin = interpolate_B(theta_inside,R_inside,Z_inside,dat_r[:,1:],indices,m,np.pi/2)
+    _,_, B_r_RZ_downsamp_sin = interpolate_B(theta_inside,R_inside,Z_inside,dat_r[:,0:],indices,m,np.pi/2)
     _,_, B_th_RZ_downsamp_cos = interpolate_B(theta_inside,R_inside,Z_inside,dat_th[:,0:],indices,m)
-    _,_, B_th_RZ_downsamp_sin = interpolate_B(theta_inside,R_inside,Z_inside,dat_th[:,1:],indices,m,np.pi/2)
+    _,_, B_th_RZ_downsamp_sin = interpolate_B(theta_inside,R_inside,Z_inside,dat_th[:,0:],indices,m,np.pi/2)
 
     ###########################
     # Plot the results
@@ -196,7 +189,7 @@ def map_psi_to_equilibrium(br_file='br_0000',bth_file='bth_0000',\
         ax[1].set_title(r'B$_\theta(\psi_N)$')
         ax[0].set_ylabel(r'Z [m]')
         for i in range(2):ax[i].set_xlabel(r'R [m]')
-        fig.savefig('../output_plots/FAR3D_Eigenfunction_Equilibrium.pdf',transparent=True)
+        fig.savefig(plot_directory+'FAR3D_Eigenfunction_Equilibrium.pdf',transparent=True)
         plt.show()
 
     print('halt')
